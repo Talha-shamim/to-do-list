b325bc3b5c823f90e180ef7ed24aed769b0e697c
Talha-shamim <Talhashamim001@gmail.com>
2020-06-03 11:25:59 +0530

    Updates

---
+++ precache-manifest.5baa65b165c0a0e6c56441fb59507597.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f93f476647db50256fba3772f304fc70",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "1c7ed23bcb03ad606f1d",
+    "url": "/to-do-list/static/css/main.c30d2ad5.chunk.css"
+  },
+  {
+    "revision": "40170bacae98b12b9b60",
+    "url": "/to-do-list/static/js/2.c3b52773.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list/static/js/2.c3b52773.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c7ed23bcb03ad606f1d",
+    "url": "/to-do-list/static/js/main.47f48c3b.chunk.js"
+  },
+  {
+    "revision": "2cd993c296c40ced4010",
+    "url": "/to-do-list/static/js/runtime-main.a52fa563.js"
+  }
+]);
